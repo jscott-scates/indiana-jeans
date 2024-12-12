d1c50db00720f37ab81ffc41b28d2f52902961ec
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2024-12-12 11:40:50 -0500

    Add files via upload

---
+++ scripts/SaveSubmission.js
@@ -0,0 +1,18 @@
+import { saveSurveySubmission} from "./TransientState.js"
+
+
+     const handleSurveySubmissionClick = (clickEvent) => {
+        if (clickEvent.target.id === "saveSubmission") {
+            saveSurveySubmission()       
+        }
+    }
+
+export const saveSubmission = () => {
+
+    document.addEventListener(
+        "click",
+        handleSurveySubmissionClick
+    )
+
+    return "<div><button id='saveSubmission'> Save Survey Responses </button></div>"
+}
